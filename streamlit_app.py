d75005329bd7aa399c122f1b37a531923e7d1195
vellampalli79 <97199792+vellampalli79@users.noreply.github.com>
2023-01-24 13:38:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,13 @@ import streamlit  as st
 
 st.title(' 🥗 🐔 🥑🍞 My parents healthy new dinner!')
 
+st.header('Breakfast Favorites')
+
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗  Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard Boilded Free-Range Eggs')
+
+
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
  
