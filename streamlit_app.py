10e3560244bdf787fb446dc96c88dad6380578d5
vellampalli79 <97199792+vellampalli79@users.noreply.github.com>
2023-01-24 14:28:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ st.text('🐔 Hard Boilded Free-Range Eggs')
 st.header('🍌🍞 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=my_fruit_list.set_index('Fruit')
 
 #Pick fruits from a list
 st.multiselect("Pick Some Fruits:",list(my_fruit_list.index))
