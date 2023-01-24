c8d54a02e0a785178c6f2e58903fe97822472b79
vellampalli79 <97199792+vellampalli79@users.noreply.github.com>
2023-01-24 13:45:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,6 @@ st.header('🍌🍞 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
 
+my_test_file = pandas.read_csv("C:\Users\F618004\Downloads\veggie_details_a_to_k_comma_opt_enclosed (2).csv")
+st.dataframe(my_test_file)
  
