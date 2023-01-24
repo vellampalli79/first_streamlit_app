13db098c980a7aa96d8e88455b060ac94571a996
vellampalli79 <97199792+vellampalli79@users.noreply.github.com>
2023-01-24 13:38:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,12 @@ st.title(' 🥗 🐔 🥑🍞 My parents healthy new dinner!')
 
 st.header('Breakfast Favorites')
 
-st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🍞 Omega 3 & Blueberry Oatmeal')
 st.text('🥗  Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard Boilded Free-Range Eggs')
 
 
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌🍞 Build Your Own Fruit Smoothie 🥝🍇')
 
  
 
