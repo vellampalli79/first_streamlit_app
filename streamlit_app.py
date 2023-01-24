6cf18766167a963676b0e9cfb9525a186b8408e4
vellampalli79 <97199792+vellampalli79@users.noreply.github.com>
2023-01-24 13:43:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.text('🐔 Hard Boilded Free-Range Eggs')
 
 st.header('🍌🍞 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv(" https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
 
  
